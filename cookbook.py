1a096c2b7b7dc26ae0dac2a54268f8ee8e512cd8
Tom B <53711814+tombalcombe@users.noreply.github.com>
2019-08-11 18:19:34 +0100

    Update cookbook.py

--- cookbook.py
+++ cookbook.py
@@ -12,14 +12,14 @@ Created on Sun Aug 11 15:17:36 2019
 
 import csv
 from copy import copy
+import time
 
 recipe = []
+instructions = []
 ingredients = []
 nest = []
 
 def Start():
-    print(recipe)
-    print(ingredients)
     print("== Cook Book ==")
     print("Please choose from the following options...")
     print("c - to create a recipe")
@@ -36,14 +36,20 @@ def Start():
         exportRecipe()
     if x == "i":
         importRecipe()
+    if x == "p":
+        printRecipe()
     else:
         quit()
         
 def createRecipe1():
+    global recipe
+    global nest
     print("Create Recipe!")
     x = input("First, type in the name of your Recipe:" )
-    recipe.append(x)
+    nest.append(str(x))
     print("Your recipe is called "+x+"! Now let's talk about ingredients.")
+    recipe.append(copy(nest))
+    nest.clear()
     createRecipe2()
     
 def createRecipe2():
@@ -57,23 +63,53 @@ def createRecipe2():
     else:
         ingredients.append(copy(nest))
         nest.clear()
-        print(ingredients)
-        Start()
+    createRecipe3()
+        
+def createRecipe3():
+    global ingredients
+    global nest
+    x = input("Now please type in the instructions:" )
+    nest.append(str(x))
+    x = input("Ok, "+x+"... now would you like to add additional instructions? y/n:")
+    if x == "y":
+        createRecipe3()
+    else:
+        instructions.append(copy(nest))
+        nest.clear()
+    Start()
+        
 
 def selectRecipe1():
-    x = input("What recipe would you like to pick?")
+    xa = input("What recipe would you like to pick?")
+    x = str(xa)
     recipeindex = 0
     for a in recipe[recipeindex:]:
-        if a == x:
-            print("You selected "+a+".")
-            print("This is recipe number "+str(recipeindex)+".")
-            break
+        for i in a:
+            print(i)
+            time.sleep(1)
+            if i == x:
+                print("You selected "+i+".")
+                print("This is recipe number "+str(recipeindex)+".")
+                break
         else:
             recipeindex = recipeindex + 1
     for a in ingredients[recipeindex:]:
         print("The ingredients for this recipe are....")
-        print(a)
+        print("...")
+        for i in a:
+            print("")
+            print(i)
+            time.sleep(0.5)
         break
+    for a in instructions[recipeindex:]:
+        print("The instructions for this recipe are....")
+        print("...")
+        for i in a:
+            print("")
+            print(i)
+            time.sleep(1)
+        break
+    print("---------------")
     Start()
     
 def exportRecipe():
@@ -86,6 +122,10 @@ def exportRecipe():
     with open('ingredients.csv', 'w') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerows(ingredients)
+    csvFile.close()
+    with open('instructions.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(instructions)
     csvFile.close()
     Start()
     
@@ -100,6 +140,15 @@ def importRecipe():
         reader = csv.reader(f)
         for row in reader:
             ingredients.append(row)
+    with open("instructions.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            instructions.append(row)
+    Start()
+    
+def printRecipe():
+    print(recipe)
+    print(ingredients)
     Start()
 
 Start()
